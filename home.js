abe164317352c66f10644705dc0c1df781ee0b18
drea <amejinight@gmail.com>
2023-08-10 15:08:55 -0500

    more fixes

---
+++ home.js
@@ -0,0 +1,5 @@
+var menuContainer = document.getElementById('home');
+var menuLink = document.createElement('a');
+menuLink.textContent = 'Home';
+menuLink.textContent = 'https://abourque72.github.io';
+menuContainer.appendChild(menuLink);
